447c8adad35f41ba533c62338690270e81b07649
jcalbert <jxa357@psu.edu>
2017-11-04 18:28:44 -0400

    Test script now sends times, testing for latency

--- netcomm.py
+++ netcomm.py
@@ -6,10 +6,25 @@ Created on Sat Nov  4 15:37:07 2017
 @author: jcalbert
 """
 
+"""
+MSG TYPES:
+
+1: I am sending a signal
+
+2: I recieved a signal    
+    
+"""
+
+
+
+
 MULTICAST_ADDR = ('224.0.0.1',8005)
 
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
+import json
+from time import time
+import sys
 
 class MulticastListener(DatagramProtocol):
     def startProtocol(self):
@@ -20,18 +35,23 @@ class MulticastListener(DatagramProtocol):
     #Run when data comes in
     def datagramReceived(self, datagram, address):
         #For now, just print
-        print datagram
+        print "Got data from {} at local time: \n{}".format(address, repr(time()))
+        print "Data: {}".format(datagram)
+        print "-"*40
 #        #eventually, decide:
          # see if this is an "I will send" message or an "I received" message
          # process that data
-
-#Run the listener
-reactor.listenMulticast(MULTICAST_ADDR[1], MulticastListener())
-reactor.run()
-
-
+         
 class MulticastSender(DatagramProtocol):
     pass
 
-sender = reactor.listenUDP(0, MulticastSender())
-sender.write('test message',MULTICAST_ADDR)
+
+if sys.argv[-1] == '-s':
+    print "Starting listener"
+    reactor.listenMulticast(MULTICAST_ADDR[1], MulticastListener())
+    reactor.run()
+
+elif sys.argv[-1] == '-c':
+    print "Starting sender"
+    sender = reactor.listenUDP(0, MulticastSender())
+    sender.write(repr(time()),MULTICAST_ADDR)
